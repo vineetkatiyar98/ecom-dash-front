c84815830e480a20e7b3f39cc51aacd37ae917a7
vineetkatiyar98 <104678201+vineetkatiyar98@users.noreply.github.com>
2023-04-26 10:03:46 +0530

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = ()=>{
+    return(
+        <div style={{fontSize: "20px" }} className='footer'>
+            <h3>this is footer page</h3>
+        </div>
+    )
+}
+export default Footer
